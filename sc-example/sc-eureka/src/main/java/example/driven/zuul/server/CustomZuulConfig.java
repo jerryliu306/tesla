4afbd3ff5273b81a108b37af66aff970a0271b3a
jerry <1031757312@qq.com>
2021-07-18 21:29:26 +0800

    add zuul custom dynamic route

---
+++ sc-example/sc-eureka/src/main/java/example/driven/zuul/server/CustomZuulConfig.java
@@ -0,0 +1,25 @@
+package example.driven.zuul.server;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+public class CustomZuulConfig {
+    @Autowired
+    private ZuulProperties zuulProperties;
+    @Autowired
+    private ServerProperties server;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Bean
+    public CustomRouteLocator routeLocator() {
+        CustomRouteLocator routeLocator = new CustomRouteLocator(this.server.getServletPrefix(), this.zuulProperties);
+        routeLocator.setJdbcTemplate(jdbcTemplate);
+        return routeLocator;
+    }
+}
